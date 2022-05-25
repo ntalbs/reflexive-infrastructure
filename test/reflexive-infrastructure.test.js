cfd4a9075db21925c0be35e71434d389bb66b8dd
ntalbs <ntalbsen@gmail.com>
2022-05-25 08:50:52 +0100

    Initial commit

---
+++ test/reflexive-infrastructure.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ReflexiveInfrastructure = require('../lib/reflexive-infrastructure-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/reflexive-infrastructure-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ReflexiveInfrastructure.ReflexiveInfrastructureStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
